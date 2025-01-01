bbeb1c70f9838a84f0ca256b027e4250b3663ded
Shozab <shozabali06@gmail.com>
2025-01-01 20:49:04 +0500

    initialize frontend project with React, Vite, and Tailwind CSS; add basic structure and components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Application from './pages/Application'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/apply-job/:id' element={<ApplyJob/>} />
+        <Route path='/applicatons' element={<Application/>} />
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
