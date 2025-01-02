6987ce2998f3cd8ccb9159d041d2f91dc645d9da
Shozab <shozabali06@gmail.com>
2025-01-02 14:23:45 +0500

    enhance Application page with resume upload functionality and job application listing; improve App component formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import ApplyJob from './pages/ApplyJob'
-import Application from './pages/Application'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ApplyJob from "./pages/ApplyJob";
+import Application from "./pages/Application";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/apply-job/:id' element={<ApplyJob/>} />
-        <Route path='/applicatons' element={<Application/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Application />} />
       </Routes>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
