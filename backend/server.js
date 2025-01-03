41e794e61413e4228ea3b6ea1a7f7a70bda0dde7
Shozab <shozabali06@gmail.com>
2025-01-03 17:21:10 +0500

    initialize backend with Express, MongoDB connection, and Sentry integration; set up basic server and middleware

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import "./configs/instrument.js";
+import * as Sentry from "@sentry/node";
+
+// Initialize Express
+const app = express();
+
+// Connect to Database
+await connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+// Port
+const port = process.env.PORT || 3000;
+
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
